1996b9c94e17cb27d535aeb5e57c410807c19c05
Thor Tepper-Garcia <ttepperg@gmail.com>
2021-12-22 09:27:52 +1100

    Changed again stdout about mass refinement (I cannot seem to get it right)

--- patch/tepper/amr/units.f90
+++ patch/tepper/amr/units.f90
@@ -58,9 +58,12 @@ subroutine units(scale_l,scale_t,scale_d,scale_v,scale_nH,scale_T2)
     write(*,'(a32,1pe10.2)') "Unit temperature/mu [K]: ", scale_T2
     write(*,'(a32,1pe10.2)') "Unit hydrogen density [cm^-3]: ", scale_nH
     write(*,*)
-    if(hydro)&
-        &write(*,'(a32,1pe10.2)') "Refinement > gas mass [Msun]: ",&
-        & m_refine(nlevelmax) * mass_sph * scale_m / M_sun
+    if (hydro) then
+      write(*,'(a32,1pe10.2)') "Refinement > gas mass [Msun] at levelmin: ",&
+      & m_refine(levelmin) * mass_sph * scale_m / M_sun
+      write(*,'(a32,1pe10.2)') "Refinement > gas mass [Msun] at nelevelmax: ",&
+	  & m_refine(nlevelmax) * mass_sph * scale_m / M_sun
+	end if
     write(*,*)
     write(*,*)
   end if
