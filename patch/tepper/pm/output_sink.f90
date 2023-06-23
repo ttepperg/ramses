2418420372fa007d2ee30b7bec9203119242104a
Thor Tepper-Garcia <ttepperg@gmail.com>
2023-06-23 14:22:26 +1000

    changed (again) output file header string 'msink' to 'mass' for convenience when analysing sims with Pynbody

--- patch/tepper/pm/output_sink.f90
+++ patch/tepper/pm/output_sink.f90
@@ -13,7 +13,8 @@ subroutine output_sink_csv(filename)
   !======================
   ! Write sink properties
   !======================
-  write(123,'(" # id,msink,x,y,z,vx,vy,vz,lx,ly,lz,tform,acc_rate,del_mass,rho_gas,cs**2,etherm,vx_gas,vy_gas,vz_gas,mbh,dmfsink,level ")')
+  !mass == msink
+  write(123,'(" # id,mass,x,y,z,vx,vy,vz,lx,ly,lz,tform,acc_rate,del_mass,rho_gas,cs**2,etherm,vx_gas,vy_gas,vz_gas,mbh,dmfsink,level ")')
   write(123,'(" # 1,m,l,l,l,l t**-1,l t**-1,l t**-1,m l**2 t**-1,m l**2 t**-1,m l**2 t**-1,t,m t**-1,m,m l**-3,l**2 t**-2,m l**2 t**-2,l t**-1,l t**-1,l t**-1,m,m,1")')
   do isink=1,nsink
      write(123,'(I10,21(A1,ES21.10),A1,I10)')idsink(isink),',',msink(isink),&
