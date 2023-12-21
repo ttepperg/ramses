78e5d7d3846d8090e32ef4e5b2541c4f7596b2d9
Thor Tepper-Garcia <ttepperg@gmail.com>
2023-12-22 10:30:18 +1100

    Applied the most recent changes to the DICE patch

--- patch/tepper/oagertz/dice/add_list.f90
+++ patch/tepper/oagertz/dice/add_list.f90
@@ -2,13 +2,13 @@
 !################################################################
 !################################################################
 !################################################################
-subroutine add_list(ind_part,list2,ok,np)
+subroutine add_list(ind_part,ind_grid,ok,np)
   use amr_commons
   use pm_commons
   implicit none
-  integer::np
-  integer,dimension(1:nvector)::ind_part,list2
-  logical,dimension(1:nvector)::ok
+  integer, intent(in)::np
+  integer,dimension(1:nvector), intent(in)::ind_part,ind_grid
+  logical,dimension(1:nvector), intent(in)::ok
   !
   ! Add particles to their new linked lists
   !
@@ -16,20 +16,20 @@ subroutine add_list(ind_part,list2,ok,np)
 
   do j=1,np
      if(ok(j))then
-        if(numbp(list2(j))>0)then
+        if (numbp(ind_grid(j)) > 0) then
            ! Add particle at the tail of its linked list
-           nextp(tailp(list2(j)))=ind_part(j)
-           prevp(ind_part(j))=tailp(list2(j))
+           nextp(tailp(ind_grid(j))) = ind_part(j)
+           prevp(ind_part(j)) = tailp(ind_grid(j))
            nextp(ind_part(j))=0
-           tailp(list2(j))=ind_part(j)
-           numbp(list2(j))=numbp(list2(j))+1
+           tailp(ind_grid(j)) = ind_part(j)
+           numbp(ind_grid(j)) = numbp(ind_grid(j)) + 1
         else
            ! Initialise linked list
-           headp(list2(j))=ind_part(j)
-           tailp(list2(j))=ind_part(j)
+           headp(ind_grid(j)) = ind_part(j)
+           tailp(ind_grid(j)) = ind_part(j)
            prevp(ind_part(j))=0
            nextp(ind_part(j))=0
-           numbp(list2(j))=1
+           numbp(ind_grid(j)) = 1
         end if
      end if
   end do
@@ -45,8 +45,8 @@ subroutine add_free(ind_part,np)
   use hydro_parameters,only: nmetals ! ERIC
   use dice_commons
   implicit none
-  integer::np
-  integer,dimension(1:nvector)::ind_part
+  integer, intent(in)::np
+  integer,dimension(1:nvector), intent(in)::ind_part
   !
   ! Add particles to the free memory linked list
   ! and reset all particle variables
@@ -56,12 +56,12 @@ subroutine add_free(ind_part,np)
 
   do idim=1,ndim
      do j=1,np
-        xp(ind_part(j),idim)=0.0
-        vp(ind_part(j),idim)=0.0
+        xp(ind_part(j),idim)=0
+        vp(ind_part(j),idim)=0
      end do
   end do
   do j=1,np
-     mp(ind_part(j))=0.0
+     mp(ind_part(j))=0
      idp(ind_part(j))=0
      levelp(ind_part(j))=0
      typep(ind_part(j))%family=FAM_UNDEF
@@ -130,14 +130,14 @@ subroutine add_free_cond(ind_part,ok,np)
   do idim=1,ndim
      do j=1,np
         if(ok(j))then
-           xp(ind_part(j),idim)=0.0
-           vp(ind_part(j),idim)=0.0
+           xp(ind_part(j),idim)=0
+           vp(ind_part(j),idim)=0
         endif
      end do
   end do
   do j=1,np
      if(ok(j))then
-        mp(ind_part(j))=0.0
+        mp(ind_part(j))=0
         idp(ind_part(j))=0
         levelp(ind_part(j))=0
         typep(ind_part(j))%family = FAM_UNDEF
