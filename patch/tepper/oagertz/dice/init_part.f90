7fc9710f45d093a5a3c79c866e6de4091377b3ab
Thor Tepper-Garcia <ttepperg@gmail.com>
2024-03-05 18:57:25 +1100

    Tiny typo in comment

--- patch/tepper/oagertz/dice/init_part.f90
+++ patch/tepper/oagertz/dice/init_part.f90
@@ -1658,7 +1658,7 @@ contains
                       ! Particle metallicity
                       if(metal) then
 !                         zp(ipart)  = zz(i)
-! TTG: The following modification is relevant for gas particles only and is adapted from /Users/tepper/codes/ramses_agertz/ramses/patch/disc/condinit.f90; it assumes nmetals = 2 (note tha zz is already scaled by ic_scale_metal):
+! TTG: The following modification is relevant for gas particles only and is adapted from /Users/tepper/codes/ramses_agertz/ramses/patch/disc/condinit.f90; it assumes nmetals = 2 (note that zz is already scaled by ic_scale_metal):
                          ! Z ~ 2O+1Fe, Madau
                          zp(ipart,1)  = ic_scale_metalFe * zz(i) !Iron
                          zp(ipart,2)  = ic_scale_metalO  * zz(i) !Oxygen
